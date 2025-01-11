32cc313d54f4d09219073dc8a36166f1561619cc
cynthia.costa <cynthia.costa@quintoandar.com.br>
2025-01-11 12:12:52 -0300

    feat: implement tokens

---
+++ src/tokens/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primary: {
+    green: '#2CE17A',
+    blue: '#356CFC',
+    dark: '#000000',
+  },
+  secondary: {
+    pink: {
+      default: '#FF3195',
+      light: '#FFB8DA',
+    },
+    red: '#FF0D0D',
+  },
+  neutral: {
+    white: '#FFFFFF',
+    offWhite: '#F5F5F5',
+  },
+};
---
+++ src/tokens/spacing.ts
@@ -0,0 +1,12 @@
+export const spacing = {
+  hxxsmall: '8px',
+  xxsmall: '20px',
+  xsmall: '24px',
+  small: '32px',
+  medium: '48px',
+  large: '56px',
+  xlarge: '64px',
+  xxlarge: '80px',
+  hxxlarge: '90px',
+  huge: '120px',
+};
